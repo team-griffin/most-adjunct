5ea5831dbda87f5a8d1fdf81e0df52b18d65d7c0
Joshua Thornton <joshua.thornton@heg.com>
2017-08-11 15:04:00 +0100

    fix: Exporting into public api missing functions.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+import fromEagerPromise from './fromEagerPromise';
 import ignoreElements from './ignoreElements';
+import mapError from './mapError';
 import switchMap from './switchMap';
+import tapError from './tapError';
 
 export {
+  fromEagerPromise,
   ignoreElements,
+  mapError,
   switchMap,
+  tapError,
 };
 
 export default {
+  fromEagerPromise,
   ignoreElements,
+  mapError,
   switchMap,
+  tapError,
 };
