5f3cb844ed24d56ce04f85decefdd3d6b1e0e0d7
Jack Ellis <jack.ellis@godaddy.com>
2017-09-29 14:23:23 +0100

    fixed typo

--- src/waitUntil.js
+++ src/waitUntil.js
@@ -2,24 +2,22 @@ import { unfold } from 'most';
 import { equals } from 'ramda';
 
 const waitUntil = (f, interval = 500) => {
-  return most
-    .unfold((done) => {
-
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          Promise
-            .resolve(f())
-            .then((value) => {
-              resolve({
-                value,
-                seed: value,
-                done,
-              });
+  return unfold((done) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        Promise
+          .resolve(f())
+          .then((value) => {
+            resolve({
+              value,
+              seed: value,
+              done,
             });
-        }, interval);
-      });
-    }, false)
-    .filter(equals(true));
+          });
+      }, interval);
+    });
+  }, false)
+  .filter(equals(true));
 };
 
 export default waitUntil;
